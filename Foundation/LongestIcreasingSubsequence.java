d43aa22a179e3acb15141fc90e5432504af12c6c
Pavi <pavittarkumarazad1@gmail.com>
2021-03-05 19:57:39 +0530

    DP

--- Foundation/LongestIcreasingSubsequence.java
+++ Foundation/LongestIcreasingSubsequence.java
@@ -1,3 +1,4 @@
+//2D DP array approach
 import java.io.*;
 import java.util.*;
 
@@ -40,3 +41,52 @@ public class Main {
     }
 
 }
+
+// Another Approach ... for 1D array of DP
+
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+
+//     public static void main(String[] args) throws Exception {
+//         // write your code here
+//         Scanner scn = new Scanner(System.in);
+//         int n = scn.nextInt();
+//         int[] arr = new int[n];
+//         for(int i = 0; i < n; i++){
+//             arr[i] = scn.nextInt();
+//         }
+        
+//         int ans = 0, max = 0;
+        
+//         for(int i = 0; i < arr.length; i++){
+            
+//             int[] dp = new int[arr.length + 1];
+//             for(int j = 0; j < dp.length; j++) dp[j] = -1;
+//             ans = solve(arr, i, dp);
+            
+//             max = Math.max(ans, max); 
+//         }
+        
+//         System.out.print(max);
+        
+//     }
+    
+//     public static int solve(int[] arr, int idx, int[] dp){
+        
+//         if(idx == arr.length) return 0;
+        
+//         if(dp[idx] != -1) return dp[idx];
+        
+//         int ans = 0;
+        
+//         for(int i = idx + 1; i < arr.length; i++){
+//             if(arr[i] > arr[idx]){
+//                 ans = Math.max(ans, solve(arr, i, dp));
+//             }
+//         }
+//         return dp[idx] = ans + 1;
+//     }
+
+// }
