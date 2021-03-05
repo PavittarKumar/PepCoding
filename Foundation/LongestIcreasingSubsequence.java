9aabc0c6d532d02e8687de25ec56a30d55f198d2
Pavi <pavittarkumarazad1@gmail.com>
2021-03-05 19:12:07 +0530

    DP

--- Foundation/LongestIcreasingSubsequence.java
+++ Foundation/LongestIcreasingSubsequence.java
@@ -12,19 +12,30 @@ public class Main {
             arr[i] = scn.nextInt();
         }
         
-        System.out.print(solve(arr, 0, -1));
+        int[][] dp = new int[arr.length + 1][arr.length + 1];
+        
+        for(int i = 0; i <= arr.length; i++){
+            for(int j = 0; j <= arr.length; j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        System.out.print(solve(arr, 0, -1, dp));
         
     }
     
-    public static int solve(int[] arr, int idx, int lastIndex){
+    public static int solve(int[] arr, int idx, int lastIndex, int[][] dp){
         if(idx == arr.length) return 0;
         
-        int f1 = solve(arr, idx + 1, lastIndex);
+        if(lastIndex != -1 && dp[idx][lastIndex] != -1) return dp[idx][lastIndex];
+        
+        int f1 = solve(arr, idx + 1, lastIndex, dp);
         int f2 = 0;
         if(lastIndex == -1 || arr[idx] > arr[lastIndex]){
-            f2 = 1 + solve(arr, idx + 1, idx);
+            f2 = 1 + solve(arr, idx + 1, idx, dp);
         }
         int ans = Math.max(f1, f2);
+        if(lastIndex != -1) dp[idx][lastIndex] = ans;
         return ans;
     }
 
