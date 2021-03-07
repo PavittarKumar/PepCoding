092a02b8457412eb10d0a007f557aec9ba8db1d7
Pavi <pavittarkumarazad1@gmail.com>
2021-03-07 22:01:42 +0530

    DP and greedy

--- Foundation/LongestPalindromicSubstring.java
+++ Foundation/LongestPalindromicSubstring.java
@@ -56,7 +56,50 @@ public class Main {
     }
 }
 
+// DP
 
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+
+//     public static void main(String[] args) throws Exception {
+//         Scanner scn = new Scanner(System.in);
+//         String str = scn.nextLine();
+//         System.out.println(cpss(str));
+//     }
+    
+//     public static int cpss(String str){
+//         int n = str.length();
+//         boolean[][] dp = new boolean[n][n];
+//         int ans = 0;
+        
+//         for(int diag = 0; diag < n; diag++){
+//             for(int i = 0, j = diag; j < n; i++,j++){
+//                 if(diag == 0){
+//                     dp[i][j] = true;
+//                 }else if(diag == 1){
+//                     if(str.charAt(i) == str.charAt(j)){
+//                         dp[i][j] = true;
+//                     }
+//                 }else{
+//                     if(str.charAt(i) == str.charAt(j) && dp[i + 1][j - 1] == true){
+//                         dp[i][j] = true;
+//                     }
+//                 }
+//                 if(dp[i][j] == true){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+        
+        
+//     }
+    
+    
+
+// }
 
 
 
