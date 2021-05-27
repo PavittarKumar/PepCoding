b898c0adf872611c81fcd438b6acb59056e3ee2c
Pavittar Kumar Azad <pavittarkumarazad1@gmail.com>
2021-05-27 14:11:11 +0530

    updated

--- LevelUp/NqueensCombinations2dAs1dQueenChooses.java
+++ LevelUp/NqueensCombinations2dAs1dQueenChooses.java
@@ -2,77 +2,62 @@ import java.io.*;
     
 public class NqueensCombinations2dAs1dQueenChooses {
 
-    public static boolean IsQueenSafe(boolean[][] chess, int row, int col) {
-        // write your code here
-        for(int i = 0; i < col; i++) {
-            if(chess[row][i]) return false;
-        }
+    public static boolean IsQueenSafe(int[][] chess, int row, int col) {
         
-        for(int i = 0; i < row; i++) {
-            if(chess[i][col]) return false;
-        }
+        int[][] dir = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
         
-        int i = row - 1;
-        int j = col - 1;
-        
-        while(i >= 0 && j >= 0) {
-            if(chess[i--][j--]) return false;
-        }
-        
-        i = row - 1;
-        j = col + 1;
-        
-        while(i >= 0 && j < chess.length) {
-            if(chess[i--][j++]) return false;
-        }
-        
-        i = row + 1;
-        j = col + 1;
-        
-        while(i < chess.length && j < chess.length) {
-            if(chess[i++][j++]) return false;
-        }
-        
-        i = row + 1;
-        j = col - 1;
-        
-        while(i < chess.length && j >= 0) {
-            if(chess[i++][j--]) return false;
+        for(int i = 1; i < chess.length; i++) {
+            
+            for(int j = 0; j < dir.length; j++) {
+                
+                int r = row + dir[j][0] * i;
+                int c = col + dir[j][1] * i;
+                
+                if(r < chess.length && r >= 0 && c < chess.length && c >= 0) {
+                    if(chess[r][c] != 0) return false;
+                }
+                
+            }
+            
         }
         
         return true;
         
     }
 
-    public static void nqueens(int qpsf, int tq, boolean[][] chess, int lcno) {
-        if (qpsf == tq) {
-            for (int row = 0; row < chess.length; row++) {
-                for (int col = 0; col < chess.length; col++) {
-                    System.out.print(chess[row][col] ? "q\t" : "-\t");
-                }
-                System.out.println();
+    public static void nqueens(int qpsf, int tq, int[][] chess) {
+        // write your code here
+        
+        if(qpsf == tq) {
+            
+            for(int i = 0; i < chess.length * chess.length; i++) {
+                int row = i/chess.length;
+                int col = i % chess.length;
+                System.out.print(chess[row][col] != 0 ? "q" + chess[row][col] + "\t" : "-\t");
+                if(col == chess.length - 1) System.out.print("\n");
             }
-            System.out.println();
+            System.out.print("\n");
             return;
         }
-
-        for (int i = lcno + 1; i < chess.length * chess.length; i++) {
-            int row = i / chess.length;
+        
+        for(int i = 0; i < chess.length * chess.length; i++) {
+            int row = i/chess.length;
             int col = i % chess.length;
-
-            if (chess[row][col] == false && IsQueenSafe(chess, row, col)) {
-                chess[row][col] = true;
-                nqueens(qpsf + 1, tq, chess, row * chess.length + col);
-                chess[row][col] = false;
-            }
+            if(chess[row][col] == 0 && IsQueenSafe(chess, row, col)) {
+                    chess[row][col] = qpsf + 1;
+                    nqueens(qpsf + 1, tq, chess);
+                    chess[row][col] = 0;
+                }
         }
+        
+        
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        boolean[][] chess = new boolean[n][n];
+        int[][] chess = new int[n][n];
 
-        nqueens(0, n, chess, -1);
+        nqueens(0, n, chess);
     }
 }
